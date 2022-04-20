6487988bfe5def394f6efdb5e35edd18d641f5bd
dddarkside <81856410+dddarkside@users.noreply.github.com>
2022-04-20 20:19:33 +0300

    includes + setup()

---
+++ def.h
@@ -0,0 +1,28 @@
+extern "C"
+{
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MK64F12.h"
+#include "fsl_debug_console.h"
+
+#include "Modules/mSpi.h"
+#include "Modules/mDac.h"
+#include "Modules/mAccelMagneto.h"
+#include "Modules/mGyro.h"
+#include "Modules/mTimer.h"
+#include "Modules/mCpu.h"
+#include "Modules/mSwitch.h"
+#include "Modules/mLeds.h"
+#include "Modules/mAd.h"
+#include "Modules/mDelay.h"
+#include "Modules/mRS232.h"
+#include "Modules/mVL6180x.h"
+}
+
+void setup_mk();
